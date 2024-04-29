c5d1873bed768154d51655fc459cfac0217ca6a9
Pelangi <pelangimasita18@gmail.com>
2024-04-29 10:52:01 +0700

    Initial Commit

---
+++ carbonPrint/carbonPrintApp.swift
@@ -0,0 +1,17 @@
+//
+//  carbonPrintApp.swift
+//  carbonPrint
+//
+//  Created by Pelangi Masita Wati on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct carbonPrintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
